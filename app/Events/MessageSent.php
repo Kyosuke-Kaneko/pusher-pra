540cb73e5f72565bcd3e25d2b9e8ab4e92574d77
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2021-12-25 14:30:30 +0900

    [edit]Channnel

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -9,21 +9,26 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Message;
+use App\Models\User;
 
-class MessageSent
+class MessageSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-
+    public $user;
+    public $message;
+ 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user, Message $message)
     {
-        //
+        $this->user = $user;
+        $this->message = $message;
     }
-
+ 
     /**
      * Get the channels the event should broadcast on.
      *
@@ -31,6 +36,6 @@ class MessageSent
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new PrivateChannel('chat');
     }
 }
